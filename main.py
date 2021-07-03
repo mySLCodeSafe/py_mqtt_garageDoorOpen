86df3938ae7b9d3b3111e0068030479ffd3734a2
ShamiLakhani <62905884+ShamiLakhani@users.noreply.github.com>
2021-07-03 18:51:08 +0100

    built subscribe

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-def main():
-    print("hello world!")
+import paho.mqtt.client as mqtt
 
-if __name__ == '__main__':
-    main()
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    if rc == 0:
+        print("connected ok")
+        client.subscribe("my/test/topic")
+        client.subscribe("$SYS/#")
+    else:
+        pass
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print("Message: "+msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client("python1")
+client.on_connect = on_connect
+client.on_message = on_message
+client.tls_set()
+client.username_pw_set(username="jackANDj1ll", password="pangaeaQWSA1")
+
+print("started...")
+client.connect("d45fff2751494701a6007b82c596c0bc.s1.eu.hivemq.cloud", 8883, 60)
+client.loop_forever()
