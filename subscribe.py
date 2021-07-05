443b147c1ff1c2f1bf7f9b0fcb8380929ef60fbc
ShamiLakhani <62905884+ShamiLakhani@users.noreply.github.com>
2021-07-05 08:10:23 +0100

    Wrapped subscribe service into a class

--- subscribe.py
+++ subscribe.py
@@ -1,25 +1,36 @@
 import paho.mqtt.client as mqtt
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    if rc == 0:
-        print("connected ok")
-        client.subscribe("my/test/topic")
-        client.subscribe("$SYS/#")
-    else:
-        pass
+class SubscribeService(mqtt.Client):
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print("Message: "+msg.topic+" "+str(msg.payload))
+    def on_connect(self, mqttc, obj, flags, rc):
+        print("Connected with result code " + str(rc))
+        self.subscribe("my/test/topic")
 
-client = mqtt.Client("python1")
-client.on_connect = on_connect
-client.on_message = on_message
-client.tls_set()
-client.username_pw_set(username="", password="")
+    def on_message(self, mqttc, obj, msg):
+        print("[Recieved]: Topic: "+msg.topic+" ; retainFlag: " + str(msg.retain) + " ; message: "+str(msg.payload.decode("utf-8")))
 
-print("started...")
-client.connect("d45fff2751494701a6007b82c596c0bc.s1.eu.hivemq.cloud", 8883, 60)
-client.loop_forever()
+    def on_log(self,mqttc, obj, level, string):
+        print("[log]:" + string)
+
+    def run(self,
+            brokerhostname:str,
+            brokerport:int,
+            credusername:str,
+            credpassword:str
+            ):
+        self.tls_set()
+        #self.subscribe("my/test/topic")
+        self.username_pw_set(username=credusername, password=credpassword)
+        self.connect(brokerhostname, brokerport, 120)
+
+        rc = 0
+        while rc == 0:
+            rc = self.loop()
+        return rc
+
+
+instance1 = SubscribeService()
+
+# TODO: Change below line to read from config file.
+rc = instance1.run("[hostname]", [port num], "[broker username]", "[broker password]")
+print (str(rc))
